4c7ddad4c8059cb2d9817f06c93b0a641fac0f52
xiye99 <11921143+xiye99@user.noreply.gitee.com>
2023-03-23 23:30:45 +0800

    第一次上传：梳理UI框架结构

---
+++ test.py
@@ -0,0 +1,19 @@
+# -*- coding:utf-8 -*-
+# @Time  :2023/3/20 7:32
+# @AUTHOR:希耶谢
+"""import selenium
+# import ddt 不需要
+import xlrd
+import openpyxl
+import jsonpath
+# import allure
+import pytest"""
+
+
+'''
+Data         =====》数据存放[page分离元素，测试用例数据]
+Data_Loader  =====》自定义数据处理
+KeyWord      =====》关键字封装[浏览器相关封装，数据库操作封装]
+Page         =====》PO页面
+TestCase     =====》用例存放
+'''
